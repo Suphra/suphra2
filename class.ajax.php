1961931496aec7628ee93d8ed39ad3cf5c3a283f
suphrproject <suphraproject@gmail.com>
2016-03-27 23:23:51 +0000

    First stable Suphra2. Steering,Drive,&Blink works!

---
+++ class.ajax.php
@@ -0,0 +1,42 @@
+<?php
+// C 2016 Amir Sadeghian (root25.com)
+class AjaxFunc{
+        public function steer($dir,$delay="0"){
+		if($dir=="left"){ $dir="24"; }else{ $dir="25"; }
+                shell_exec("gpio -g mode ".$dir." out");
+                shell_exec("gpio -g write ".$dir." 1");
+		sleep($delay);
+                shell_exec("gpio -g write ".$dir." 0");
+                shell_exec("sudo python cleanup.ph");
+        }
+
+	public function drive($dir,$delay="0"){
+		if($dir=="backward"){ $dir="18"; }else{ $dir="23"; }
+                shell_exec("gpio -g mode ".$dir." out");
+                shell_exec("gpio -g write ".$dir." 1");
+                sleep($delay);
+                shell_exec("gpio -g write ".$dir." 0");
+                shell_exec("sudo python cleanup.ph");
+	}
+	
+	public function stop(){		
+                shell_exec("gpio -g mode 18 out");
+                shell_exec("gpio -g mode 23 out");
+                shell_exec("gpio -g write 18 0");
+                shell_exec("gpio -g write 23 0");
+                shell_exec("sudo python cleanup.ph");
+	}
+
+	public function blink($led,$delay="0"){
+		if($led=="1"){ $led="17"; }else{ $led="27"; }
+                shell_exec("gpio -g mode ".$led." out");
+                shell_exec("gpio -g write ".$led." 1");
+                sleep($delay);
+                shell_exec("gpio -g write ".$led." 0");
+                shell_exec("sudo python cleanup.ph");
+	}
+
+
+}
+?>
+
--- robot.php
+++ robot.php
@@ -1,20 +1,29 @@
 <?php
-//phpinfo();
-$out=shell_exec('ls -lart');
-echo "<pre>$output</pre>";
-
-//shell_exec('whoami');
-shell_exec(" sudo gpio -g mode 17 out");
-shell_exec("sudo gpio -g mode 27 out");        
-//	$setmode17=system("/sys/class/gpio -g mode 17 out");
-	//while(TRUE){
-        	$gpio_on=shell_exec("sudo gpio -g write 17 1");
-		//sleep(0.1);
-		$gpio_off=shell_exec("gpio -g write 17 0");
+require_once("class.ajax.php");
+$aF= new  AjaxFunc();
+$aF->steer("right","1");
+$aF->blink("1","2");
+$aF->drive("forward","3");
+/*
+* 18 Engine backward
+* 23 Engine Forward
+* 24 Steer left
+* 25 Steer right
+* 17 Led 1
+* 27 Led 2
+*/
+//shell_exec("python cleanup.py");
+//shell_exec("gpio -g mode 17 out");
+//shell_exec("gpio -g mode 27 out");
+//while(1){        
+	//shell_exec("gpio -g write 17 1");
+	//shell_exec("gpio -g write 27 1");
+	//sleep(1);
+//	shell_exec("gpio -g write 17 0");
+//	shell_exec("gpio -g write 27 0");
+//}
 		//$gpio_on=shell_exec("gpio -g write 27 1");
 		//sleep(0.5);
 		//shell_exec("gpio -g write 27 0");
-	//}
-echo($gpio_on);
 ?>
 
